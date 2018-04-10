26eb16734ff44a73012ba975f2e06dca55a56ad6
刘雪松 <1076239475@qq.com>
2018-04-10 17:27:30 +0800

    add

---
+++ pattern/null-object-pattern/src/main/java/NullCustomer.java
@@ -0,0 +1,11 @@
+public class NullCustomer extends  AbstractCustomer {
+    @Override
+    public boolean isNull() {
+        return true;
+    }
+
+    @Override
+    public String getName() {
+        return "Not Available in Customer Database";
+    }
+}
